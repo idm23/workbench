afb091b82142daa391107f0a14a25fcb655365ae
Workbench <workbench@homebox-core>
2026-09-21 21:19:31 +0000

    Finish PR #169: add login to run creation and display; add tests; remove root conftest

--- conftest.py
+++
@@ -1,8 +0,0 @@
-import resource
-
-# Increase the maximum number of open file descriptors to avoid
-# "Too many open files" during pytest cleanup.
-try:
-    resource.setrlimit(resource.RLIMIT_NOFILE, (resource.RLIM_INFINITY, resource.RLIM_INFINITY))
-except Exception:
-    pass
